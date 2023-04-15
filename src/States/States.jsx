14636ec682d2b7c87a3a803ac7b3bc1f6fa731a6
Zacharylutfyr <zacharylutfyr@gmail.com>
2023-04-15 16:49:55 +0700

    "Callback Function"

--- src/States/States.jsx
+++ src/States/States.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
 
 const States = () => {
-  const [statee, setState] = useState(98);
+  const [statee, setState] = useState(0);
   return (
     <>
       <h1>JUMLAH : {statee} </h1>
-      <button onClick={() => {
-        setState (2);
-      }}>Ubah</button>
+
+      {/* Memakai Callback Function agar tidak langsung jalan saat pertama kali */}
+      <button
+        onClick={() => {
+          setState(statee + 1);
+        }}>
+        Tambah
+      </button>
+      <button
+        onClick={() => {
+          setState(statee - 1);
+        }}>
+        Kurangi
+      </button>
     </>
   );
 };
