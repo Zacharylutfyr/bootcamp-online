80d70eee751eea9d7fb358b66a82077ab8b41f4d
Zacharylutfyr <zacharylutfyr@gmail.com>
2023-04-15 15:43:49 +0700

    "Learn State"

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
+import React, { Component, useState } from 'react';
 import Navbar from './components/Navbar';
 import './index.css';
 
-
-// * React Component = sebuah function / class yang mereturn JSX 
+// * React Component = sebuah function / class yang mereturn JSX
 // * 1. Namanya harus dimuai dengan huruf Kapital
 // * 2. Hanya Bisa Mereturn sebuah parent element
 // * 3. Bisa memiliki State & Props
 // * 4. Bisa di panggil di componenet lain dengan cara <Nama Component/>
 
 const App = () => {
+  const [count, setCount] = useState(0);
   return (
     <>
       <Navbar />
-      <div>
-        <h1>Title</h1>
-      </div>
+
+      <h1>Jumlah : {count}</h1>
+      <button
+        onClick={() => {
+          setCount();
+        }}>
+        Tambah
+      </button>
     </>
   );
 };
 
-export default App;
+export default App
---
+++ src/States/States.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class States extends Component {
+  render() {
+    return (
+   <h1>Zachayr</h1>
+    );
+  }
+}
+
+export default States;
