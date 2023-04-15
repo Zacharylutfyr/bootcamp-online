c3518617a8fe8721966984e46fcaf12855650581
Zacharylutfyr <zacharylutfyr@gmail.com>
2023-04-15 23:51:42 +0700

    "Add Range and Color"

--- src/States/States.jsx
+++ src/States/States.jsx
@@ -2,35 +2,64 @@ import React, { useState } from 'react';
 
 const States = () => {
   const [statee, setState] = useState(0);
-  const [text, setText] =useState ('');
+  const [text, setText] = useState('ini Default Text');
+  const [range, setRange] = useState(16);
+  const [color, setColor] = useState('oranged');
+
   return (
     <>
-      <h1>JUMLAH : {statee} </h1>
+      <div className='container'>
+        <h1>JUMLAH : {statee} </h1>
 
-      {/* Memakai Callback Function agar tidak langsung jalan saat pertama kali */}
-      <button
-        onClick={() => {
-          setState(statee + 1);
-        }}>
-        Tambah
-      </button>
-      <button
-        onClick={() => {
-          if (statee >= 1) {
-            setState(statee - 1);
-            return;
-          }
-        }}>
-        Kurangi
-      </button>
+        {/* Memakai Callback Function agar tidak langsung jalan saat pertama kali */}
+        <button
+          onClick={() => {
+            setState(statee + 1);
+          }}>
+          Tambah
+        </button>
+        <button
+          onClick={() => {
+            if (statee >= 1) {
+              setState(statee - 1);
+              return;
+            }
+          }}>
+          Kurangi
+        </button>
 
+        <h3
+          style={{
+            fontSize: range + 'px',
+            color: color,
+          }}>
+          {text}
+        </h3>
+        <input
+          type='text'
+          placeHolder='Masukkan Text'
+          onChange={e => {
+            setText(e.target.value);
+          }}
+        />
 
-<h3>Defaul Text : {text}</h3>
-<input type="text" />
-<button onClick={()=>{
-         setState();
-}}>Ubah Text</button>
-
+        <div>
+          <input
+            type='range'
+            min={1}
+            max={100}
+            onChange={e => {
+              setRange(e.target.value);
+            }}></input>
+        </div>
+        <div>
+          <input
+            type='color'
+            onChange={e => {
+              setColor(e.target.value);
+            }}></input>
+        </div>
+      </div>
     </>
   );
 };
